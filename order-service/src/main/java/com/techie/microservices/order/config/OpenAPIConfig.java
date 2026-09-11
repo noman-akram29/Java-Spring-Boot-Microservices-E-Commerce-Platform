e602db2d8cfaace999993e74c82e21a841b17354
Noman Akram <nomanakram@carecloud.com>
2026-09-11 12:23:12 +0500

    fix(gateway): align security credentials and configuration for Swagger endpoints

--- order-service/src/main/java/com/techie/microservices/order/config/OpenAPIConfig.java
+++ order-service/src/main/java/com/techie/microservices/order/config/OpenAPIConfig.java
@@ -1,21 +1,29 @@
 package com.techie.microservices.order.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
 
-	@Bean
-	public OpenAPI orderServiceAPI() {
-		return new OpenAPI()
-		       .info(new Info().title("Order Service API")
-		             .description("This is the REST API for Order Service")
-		             .version("v0.0.1")
-		             .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-	}
+    @Bean
+    public OpenAPI orderServiceAPI() {  
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .description("This is the REST API for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server()
+                                .url("/api/order")
+                                .description("via API Gateway")
+                ));
+    }
 }
--- product-service/src/main/java/com/techie/microservices/product/config/OpenAPIConfig.java
+++ product-service/src/main/java/com/techie/microservices/product/config/OpenAPIConfig.java
@@ -1,21 +1,29 @@
 package com.techie.microservices.product.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
 
-	@Bean
-	public OpenAPI productServiceAPI() {
-		return new OpenAPI()
-		       .info(new Info().title("Product Service API")
-		             .description("This is the REST API for Product Service")
-		             .version("v0.0.1")
-		             .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-	}
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product Service API")
+                        .description("This is the REST API for Product Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server()
+                                .url("/api/product")
+                                .description("via API Gateway")
+                ));
+    }
 }
